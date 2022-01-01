d48d9accc304d1b937f9252cde464fc2f04bdfa6
farshidjf <75459761+farshidjf@users.noreply.github.com>
2022-01-01 14:55:48 -0500

    Day15 speedup with heap

--- Day15/puzzle.py
+++ Day15/puzzle.py
@@ -1,35 +1,28 @@
-def grid_dijkstra(weight: list[list[int]]):
+from itertools import product
+import heapq
+def grid_dijkstra(weight: list[list[int]]) -> int:
       N = len(weight)
       start, target = (int(0), int(0)), (N-1, N-1)
       V = {(i, j) for i in range(N) for j in range(N)}
       neighbors = {v: {n for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)) 
                        if (n := (v[0]+dx, v[1]+dy)) in V}
-                   for v in V} 
-      distance = {v: 20 * N for v in V}
-      distance[start] = 0
+                   for v in V}
       visited = set()
-      unvisited = {start}
+      unvisited = [(0, start)]
       while target not in visited:
-            current = min(unvisited, key=lambda v: distance[v])
-            for n in neighbors[current] - visited:
-                  if distance[n] > (_distance := weight[n[0]][n[1]] + distance[current]):
-                        distance[n] = _distance
-                  unvisited.add(n)
-            unvisited.remove(current)
+            distance, current = heapq.heappop(unvisited)
+            if current in visited: continue
             visited.add(current)
-      return distance[target]
+            for n in neighbors[current] - visited:
+                  heapq.heappush(unvisited, (weight[n[0]][n[1]] + distance, n))
+      return distance
 
-def tile_area(tile: list[list[int]], repeat: int = 5):
+def tile_area(tile: list[list[int]], repeat: int = 5) -> list[list[int]]:
       N = len(tile)
       area = [[0 for _ in range(N*repeat)] for __ in range(N*repeat)]
-      for i in range(repeat):
-            for ii in range(N):
-                  for j in range(repeat):
-                        for jj in range(N):
-                              _value = tile[ii][jj]
-                              for _ in range(i+j):
-                                    _value = _value + 1 if _value != 9 else 1
-                              area[i * N + ii][j * N + jj] = _value
+      for i, j  in product(range(repeat), repeat=2):
+            for ii, jj in product(range(N), repeat=2):
+                  area[i * N + ii][j * N + jj] = (tile[ii][jj] + i + j - 1) % 9 + 1
       return area
 
 file_names = ['test_input', 'main_input']
@@ -38,5 +31,4 @@ for file_name in file_names[:]:
       with open(folder_name + file_name) as input_file:
             risks = [[int(r) for r in line.strip()] for line in input_file.readlines()]
             print(f'Puzzle 1 for {file_name}: {grid_dijkstra(risks)}')
-            tiled_risks = tile_area(risks)
-            print(f'Puzzle 2 for {file_name}: {grid_dijkstra(tiled_risks)}')
+            print(f'Puzzle 2 for {file_name}: {grid_dijkstra(tile_area(risks))}')
